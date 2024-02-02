9c8a12b9ddacd974d338db0ef2d7fd2f8834fa4b
Marat Karimov <marat.karimov@scalewill.com>
2024-02-02 20:36:33 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   telegram_webhook TelegramReminderController, :reminder
+  telegram_webhook TelegramReminderController, :production_reminder
 end
